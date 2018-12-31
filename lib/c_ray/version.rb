68be0c582bdb764530f06da38a81804d883416b7
Eduard Thamm <eduard.thamm@thammit.at>
2018-12-31 20:37:17 +0100

    Auto correct offenses

--- lib/c_ray/version.rb
+++ lib/c_ray/version.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 module CRay
-  VERSION = '0.0.1'.freeze
+
+  VERSION = '0.0.1'
+
 end
--- spec/c_ray_spec.rb
+++ spec/c_ray_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe CRay do
   it 'has a version number' do
     expect(CRay::VERSION).not_to be nil
